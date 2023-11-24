6cdf6f5d66a5a6f9867024735f820d33263faa5e
Vaverka123 <maslova.vera.2019@yandex.by>
2023-11-24 13:28:01 +0100

    create project

---
+++ kitchenwizard/kitchenwizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  kitchenwizardApp.swift
+//  kitchenwizard
+//
+//  Created by Vera Maslava on 24.11.23.
+//
+
+import SwiftUI
+
+@main
+struct kitchenwizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
